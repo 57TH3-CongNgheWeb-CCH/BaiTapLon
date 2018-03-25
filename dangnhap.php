cc8b493c7aea6e5f6e7c41c2ac1f236f947e538b
cuongdinh1507 <cuongdz1507@gmail.com>
2018-03-25 22:35:24 +0700

    kết nối login-signup-DB

---
+++ dangnhap.php
@@ -0,0 +1,17 @@
+<?php 
+	include("ketnoi.php");
+
+	if(isset($_POST["btn_login"])){
+		$sql = 'select * from taikhoan where Email ="'.$_POST['email'].'" and Password = "'.$_POST['pw'].'" ';
+		$result = mysqli_query($conn, $sql);
+		if(mysqli_num_rows($result) > 0){
+			echo "Đăng nhập thành công! Chào ".$_POST["email"];
+			$_SESSION['email'] = $_POST['email'];
+		}
+		else{
+			echo "ncl";
+		}
+	}
+
+	mysqli_close($conn);
+ ?>
--- login.php
+++ login.php
@@ -1,27 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
 	<title>Đăng nhập tài khoản</title>
 	<link rel="stylesheet" type="text/css" href="login.css">
 	<link rel="icon" href="cannabis.ico">	
 </head>
 <body>
-	<div id="menu">		
-		<div id="menu-bar">
-			<nav>
-				<a href="web1.php">Trang chủ</a>
-				<a href="webbenh.php">Tra cứu bệnh</a>
-				<a href="webthuoc.php" style="color: green">Tra cứu thuốc</a>
-				<a href="Lienhe.php">Liên hệ</a>
-				<a href="login.php">Đăng nhập</a>
-				<a href="signup.php">Đăng ký</a>
-								
-			</nav>
-		</div>
-	</div>	
+	<?php include("dangnhap.php"); ?>
 	<div id="logo"><a href="web1.php" ><img src="images/logo2.png"></a></div>
-	<form action="action_page.php" class="modal-content">
+	<form method="POST">
 		<div class="imgcontainer">
 			<img src="images/user-ava.png" alt="Avatar" class="avatar">
 		</div>
@@ -33,7 +21,7 @@
 			<label for="pw"><b>Mật khẩu</b></label>
 			<input type="password" placeholder="Mật khẩu" name="pw" required>
 
-			<button type="submit">Đăng nhập</button>
+			<button type="submit" name="btn_login">Đăng nhập</button>
 		</div>
 	</form>
 </body>
