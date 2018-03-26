fe3ae23349deedb92460494cb1af0ae9e43519b0
MCuong <36029979+Cuonghb1997@users.noreply.github.com>
2018-03-26 15:02:42 +0700

    login.php và signup.php cùng một div

--- appliaction/modules/Menu.php
+++ appliaction/modules/Menu.php
@@ -11,8 +11,10 @@
 				<a href="webbenh.php?webbenh1=webbenh2&id=11">Tra cứu bệnh</a>
 				<a href="index.php?Tracuuthuoc1=tracuuthuoc&id=1" style="color: green">Tra cứu thuốc</a>
 				<a href="../askForm.php">Liên hệ</a>
-				<a href="../login.php">Đăng nhập</a>
-				<a href="../signup.php">Đăng ký</a>
+				<div id="menu2" style="display: inline-block;">
+					<a href="../login.php">Đăng nhập</a>
+					<a href="../signup.php">Đăng ký</a>
+				</div>
 			</nav>
 		</div>
 	</div>
