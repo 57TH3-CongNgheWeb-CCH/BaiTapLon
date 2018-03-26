cfcb5e0c9d57d4762b4e347029d4b66f852898ad
MCuong <36029979+Cuonghb1997@users.noreply.github.com>
2018-03-26 16:02:09 +0700

    sửa file web.php webbenh.php

--- appliaction/web.php
+++ appliaction/web.php
@@ -1,7 +1,7 @@
 <?php
-	include('C:/xampp/htdocs/test/Web1/Tách riêng/header1.php');
-	include('modules/Menu.php');
-	include('modules/header.php');
-	include('C:/xampp/htdocs/test/Web1/Tách riêng/content1.php');
+	include('../Web1/Tách riêng/header1.php');
+	include('../appliaction/modules/Menu.php');
+	include('../appliaction/modules/header.php');
+	include('../Web1/Tách riêng/content1.php');
 
 ?>
--- appliaction/webbenh.php
+++ appliaction/webbenh.php
@@ -1,7 +1,7 @@
 <?php
-	include('C:/xampp/htdocs/test/Webbenh/Tách/header2.php');
-	include('modules/Menu.php');
-	include('modules/header.php');
-	include('C:/xampp/htdocs/test/Webbenh/Tách/content2.php');
-	include('C:/xampp/htdocs/test/Webbenh/Tách/footer2.php');
+	include('../Webbenh/Tách/header2.php');
+	include('../appliaction/modules/Menu.php');
+	include('../appliaction/modules/header.php');
+	include('../Webbenh/Tách/content2.php');
+	include('../Webbenh/Tách/footer2.php');
 ?>
