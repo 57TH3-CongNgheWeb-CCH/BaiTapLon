189a16e89a7123c804640441f15ad1fbea70f370
MCuong <36029979+Cuonghb1997@users.noreply.github.com>
2018-03-27 23:12:38 +0700

    viết lại phần giới thiệu trang chủ.

--- Web1/Tách riêng/content1.php
+++ Web1/Tách riêng/content1.php
@@ -5,19 +5,19 @@
 <div id="content1">
 		<div id="padding">
 			<h2>Giới thiệu</h2><br>
-			<p style="line-height: 1.8; font-size: 18px">Trang web sẽ cung cấp, chia sẻ cho bạn đọc những thông tin hữu ích, là nơi để cộng đồng cùng thảo luận về các vấn đề sức khỏe cũng như những cây thuốc bổ ích.</p>
+			<p style="line-height: 1.8; font-size: 18px">Trang web sẽ cung cấp, chia sẻ cho bạn đọc những thông tin hữu ích, là nơi để cùng thảo luận về các vấn đề sức khỏe các bệnh nguy hiểm để phòng tránh một cách tốt nhất và hiệu quả nhất cũng như những cây thuốc bổ ích có giúp chữa dứt điểm các bệnh và không để lại tác dụng phụ không mong muốn.</p>
 		</div>
 	</div>
 	<div id="content2">
 		<div id="padding">
 			<h2>Nhiệm vụ</h2><br>
-			<p style="line-height: 1.8; font-size: 18px">Giúp các bạn đọc nâng cao được kiến thức về các loại cây dân gian, có kiến thức trong việc điều trị và chữa các bệnh nguy hiểm. Cải thiện cuộc sống sức khỏe hàng ngày của bản thân và gia đình.</p>
+			<p style="line-height: 1.8; font-size: 18px">Giúp các bạn đọc nâng cao được kiến thức về các loại cây dân gian, cách dùng, liều lượng dùng nơi phân bố chủ yếu, có kiến thức trong việc điều trị và chữa các bệnh nguy hiểm mà tốn chi phí một cách thấp nhất. Đặc biệt có thể tìm ngay trong cuộc sống hàng ngày. Cải thiện cuộc sống sức khỏe về mặt con người cũng như tinh thần của bản thân và gia đình.</p>
 		</div>
 	</div>
 	<div id="content3">
 		<div id="padding">
 			<h2>Liên hệ</h2><br>
-			<p style="line-height: 1.8; font-size: 18px">Để được giải đáp các thắc mắc liên quan đến sức khỏe, cũng như những cây thuốc, bạn có thể </p>
+			<p style="line-height: 1.8; font-size: 18px">PGS.TS Nguyễn Quý Hiếu sẽ giải thích chi tiết một cách trọng tâm những câu hỏi của người đọc về những vấn đề liên quan đến sức khỏe, cũng như những cây thuốc, cách phòng bệnh khi chưa mắc phải. </p>
 		</div>
 	</div>
 	<script src="../JqueryBootstrap/bootstrap.min.js"></script>
--- Web1/trangchu.php
+++
@@ -1,5 +0,0 @@
-<?php
-	include('Tách riêng/header1.php');
-	include('Tách riêng/content1.php');
-
-?>
