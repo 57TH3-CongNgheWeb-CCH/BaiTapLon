77ebdc675e08d46f26aa3691b0baa57067d6cc50
Cường Đinh <35897209+cuongdinh1507@users.noreply.github.com>
2018-03-27 10:26:07 +0700

    commit

--- dangnhap.php
+++ dangnhap.php
@@ -6,7 +6,9 @@
 		$result = mysqli_query($conn, $sql);
 		if(mysqli_num_rows($result) > 0){
 			echo "Đăng nhập thành công! Chào ".$_POST["email"];
+
 			$_SESSION['email'] = $_POST['email'];
+			 echo "<script>console.log( 'Debug Objects: " . $_SESSION['email'] . "' );</script>";
 			header('Location: appliaction/web.php');
 		}
 		else{
