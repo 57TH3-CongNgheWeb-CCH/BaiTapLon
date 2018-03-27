35399cc8edf547b367f4a3e6bc2d430962dada5b
cuongdinh1507 <cuongdz1507@gmail.com>
2018-03-27 17:06:04 +0700

    commit

--- Lienhe.php
+++ Lienhe.php
@@ -7,44 +7,54 @@
 	<script type="text/javascript" src="lienhescript.js"></script>
 	<script type="text/javascript" src="jquery-3.3.1.min.js"></script>
 </head>
-<body>	
-	<?php
-		include("ketnoi.php");
-		if (isset($_POST["submit"])){
-			$email = $_POST['email'];
-			$comment = $_POST['comment'];
-
-			$sql = "insert into lienhe(Email, noidung) values('$email','$comment');";
-			$result = mysqli_query($conn, $sql);
-			if ($email == "" && $comment == ""){
-				echo "Không được bỏ trống! Mời bạn điền đẩy đủ thông tin";
-			}
-			else{		
-				if($result){
-					echo "Đã gửi";
-				}
-				else{
-					echo "Không thành công";
-				}
-			}
-		}
-	mysqli_close($conn);
- 	?>
+<body>
 	
+	<?php
+		// define variables and set to empty values
+		$nameErr = $emailErr = $genderErr = $websiteErr = "";
+		$name = $email = $gender = $comment = $website = "";
+
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+				  
+				  
+				  if (empty($_POST["email"])) {
+				    $emailErr = "Bắt buộc điền email";
+				  } else {
+				    $email = test_input($_POST["email"]);
+				    // check if e-mail address is well-formed
+				    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				      $emailErr = "Sai định dạng email"; 
+				    }
+				  }			    
+				  
+				  if (empty($_POST["comment"])) {
+				    $comment = "";
+				  } else {
+				    $comment = test_input($_POST["comment"]);
+				  }			  
+				}
+
+				function test_input($data) {
+				  $data = trim($data);
+				  $data = stripslashes($data);
+				  $data = htmlspecialchars($data);
+				  return $data;
+				}
+	?>
 			<div id="logo">
 				<a href="appliaction/web.php">
 					<img src="images/logo2.png">
 				</a>
 			</div><br><br><br>	
-			<form name="frm" method="POST">  
+			<form name="frm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
 				<p><span class="error">* trường bắt buộc</span></p><br>
 				<br>
-				E-mail: <input type="text" name="email" required>*
-				
+				E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+				<span class="error">* <?php echo $emailErr;?></span>
 				<br><br>
 				
 							
-				Bình luận:<br> <textarea name="comment" class="main_txt_area" rows="10" cols="100" required onkeyup="return displayWordCounter();" ></textarea>*<br>
+				Bình luận:<br> <textarea name="comment" class="main_txt_area" rows="10" cols="100" onkeyup="return displayWordCounter();"><?php echo $comment;?></textarea><br>
 				<div class="total_count">Số ký tự còn lại:
 				<input class="show_count" name="totalWordLimit" size="4" readonly="" value="1000" type="text">
 				</div>
