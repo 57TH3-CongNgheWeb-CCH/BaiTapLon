5fe6709fbd399efc3338e540d92cf7c6974dc5a3
hieunq52 <hieunq52@wru.vn>
2018-03-31 17:03:24 +0700

    update sql

--- sql/caythuoc (1).sql
+++ sql/caythuoc (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 30, 2018 lúc 03:32 PM
+-- Thời gian đã tạo: Th3 31, 2018 lúc 12:02 PM
 -- Phiên bản máy phục vụ: 10.1.30-MariaDB
 -- Phiên bản PHP: 7.2.2
 
@@ -104,6 +104,7 @@ CREATE TABLE `benh` (
 --
 
 INSERT INTO `benh` (`tenbenh`, `noidung`, `anh`) VALUES
+('', 'abc', ''),
 ('Béo phì', 'thừa cân', ''),
 ('Cao huyết áp', 'Cao huyết áp (hay còn được gọi là tăng huyết áp hay là lên tăng-xông, từ chữ Hypertension trong tiếng Pháp) là một bệnh mạn tính trong đó áp lực máu đo được ở động mạch tăng cao. Huyết áp thường được đo bằng hai chỉ số là: huyết áp tâm thu (systolic) và huyết áp tâm trương (diastolic), dựa trên 2 giai đoạn co bóp và giãn nghỉ của cơ tim, tương ứng với áp lực cao nhất và áp lực thấp nhất của dòng máu trong động mạch. Có nhiều quy chuẩn khác nhau về khoảng bình thường của huyết áp. Huyết áp lúc nghỉ thông thường nằm trong khoảng 100-140mmHg huyết áp tâm thu và 60-90mmHg huyết áp tâm trương. Bệnh nhân bị cao huyết áp khi đo huyết áp của bệnh nhân thường xuyên thấy cao hơn hoặc bằng 140/90 mmHg. Tăng huyết áp được phân loại thành tăng huyết áp nguyên phát và thứ phát. \"Tăng huyết áp nguyên phát\" chiếm 90–95% số ca tăng huyết áp, dùng để chỉ các trường hợp không xác định được bệnh nguyên gây tăng huyết áp rõ ràng (vô căn). Khoảng 5–10% số ca còn lại (tăng huyết áp thứ phát) có nguyên nhân là một số bệnh tác động lên thận, động mạch, tim, và hệ nội tiết. Tăng huyết áp gây nhiều áp lực cho tim, có khả năng dẫn đến bệnh tim do tăng huyết áp, bệnh mạch vành. Tăng huyết áp cũng là một yếu tố nguy cơ chính trong: tai biến mạch máu não, suy tim, phình động mạch, bệnh thận mạn, và bệnh động mạch ngoại biên. Ăn kiêng và thay đổi lối sống có thể cải thiện tình trạng huyết áp và giảm nguy cơ xảy ra các biến chứng, mặc dù vậy vẫn có thể cần điều trị kèm bằng thuốc ở những ca mà các biện pháp thay đổi lối sống không có tác dụng hoặc không giảm được đến huyết áp mục tiêu.\r\nĐiều chỉnh lối sống Bước đầu trong điều trị tăng huyết áp phải thay đổi lối sống hợp lý bao gồm: chế độ ăn, tập thể dục, giảm cân. Thay đổi lối sống có thể giảm huyết áp ở một số bệnh nhân. Hiệu quả của chúng tương tự như sử dụng thuốc. Nếu huyết áp vẫn cao phải sử dụng thuốc ngay, khuyến cáo điều trị ở trường hợp này là điều chỉnh lối sống kết hợp với sử dụng thuốc. Chế độ ăn ít muối rất có lợi. Nghiên cứu đối với người châu Âu (Caucasians) cho thấy nếu thực hiện chế độ ăn ít muối trong hơn 4 tuần nhận thấy sẽ giảm huyết áp, ở cả người bình thường lẫn bệnh nhân tăng huyết áp. Chế độ ăn DASH (Dietary Approaches to Stop Hypertension), chế độ ăn giàu hạt dẻ, cá, gia cầm, trái cây và rau quả giảm huyết áo. Điều quan trọng là phải hạn chế hấp thu Natri, Mặc dù chế độ ăn cũng giàu kali, magnesium, calci, protein. Những phương pháp điều trị khác hướng đến giảm những căng thẳng thần kinh như biofeedback, thư giãn hoặc thiền có thể điều trị huyết áp. Tuy nhiên, hiệu quả của chúng không bằng giáo dục chăm sóc sức khỏe, với những nhận thức những lối sống không tốt.', '../images/huyetap.jpg'),
 ('mat ngu', 'fffffffffffffff', ''),
@@ -118,24 +119,25 @@ INSERT INTO `benh` (`tenbenh`, `noidung`, `anh`) VALUES
 --
 
 CREATE TABLE `lienhe` (
+  `idlienhe` int(11) NOT NULL,
   `Email` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
-  `noidung` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+  `noidung` text CHARACTER SET utf8 COLLATE utf8_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Đang đổ dữ liệu cho bảng `lienhe`
 --
 
-INSERT INTO `lienhe` (`Email`, `noidung`) VALUES
-('hieunq52@wru.vn', 'dsfa '),
-('hieunq52@wru.vn', 'dsfa '),
-('hieunq52@wru.vn', 'a fds'),
-('hieunq52@wru.vn', 'liÃªn há»‡'),
-('hieunq52@wru.vn', 'hiáº¿u'),
-('hieunq52@wru.vn', 'hiáº¿u'),
-('hieunq52@wru.vn', 'liÃªn há»‡'),
-('hieunq52@wru.vn', 'liÃªn há»‡'),
-('abc', 'abc');
+INSERT INTO `lienhe` (`idlienhe`, `Email`, `noidung`) VALUES
+(1, 'hieunq52@wru.vn', 'dsfa '),
+(2, 'hieunq52@wru.vn', 'dsfa '),
+(3, 'hieunq52@wru.vn', 'a fds'),
+(4, 'hieunq52@wru.vn', 'liÃªn há»‡'),
+(5, 'hieunq52@wru.vn', 'hiáº¿u'),
+(6, 'hieunq52@wru.vn', 'hiáº¿u'),
+(7, 'hieunq52@wru.vn', 'liÃªn há»‡'),
+(8, 'hieunq52@wru.vn', 'liÃªn há»‡'),
+(9, 'abc', 'abc');
 
 -- --------------------------------------------------------
 
@@ -189,8 +191,8 @@ INSERT INTO `taikhoan` (`Email`, `Password`) VALUES
 
 CREATE TABLE `thuoc` (
   `idthuoc` varchar(5) NOT NULL,
-  `tenthuoc` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `noidung` text CHARACTER SET utf32 COLLATE utf32_unicode_ci,
+  `tenthuoc` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `noidung` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `anh` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -202,12 +204,11 @@ INSERT INTO `thuoc` (`idthuoc`, `tenthuoc`, `noidung`, `anh`) VALUES
 ('1', 'Atiso', 'Cây hoa atiso một loại trà, cũng là một vị thuốc nam điều trị bệnh gan rất hay. Ở bài viết này xin giới thiệu với các bạn những công dụng chính và cách dùng vị thuốc này hiệu quả nhất. Cây Atiso được đọc là (ac ti sô) một loại cây thuốc nam được sử dụng bởi người Châu Âu cách đây hàng ngàn năm. Cây Atiso (ac ti sô) có nguồn gốc từ Địa Trung Hải, đã được người Hy Lạp cổ đại trồng làm rau ăn và làm thuốc. Cây được du nhập vào Việt Nam vào những năm 1930 bởi người Pháp. Hiện nay cây Atiso (ac ti sô) được trồng nhiều ở Đà Lạt làm dược liệu, cách đây vài năm trà Atiso là một trong những đồ uống khoái khẩu của nhiều người, được dùng như một thứ đồ uống có công dụng mát gan, giải độc cho cơ thể. Bộ phận dùng: Toàn cây gồm lá, thân, rễ và cả hoa. Cách chế biến và thu hái: Có thể dùng tươi hoặc dùng khô, cây tươi dùng làm rau ăn. Ngoài ra cây còn được phơi khô làm trà và dược liệu. Hoa Atiso thường được thu hái quanh năm, hoa thu về được rửa sạch, phơi hoặc sấy khô làm thuốc. Hiện nay có nhiều công ty đã chế biến dưới dạng trà túi lọc và cao atiso rất tiện sử dụng. Thành phần hóa học: Cây Atiso có chứa nhiều hợp chất quý có lợi cho cơ thể như: Các acid hữu cơ, Flavonoid, Cynaopicrin và nhiều loại khoáng chất khác.', '../images/atiso.jpg'),
 ('2', 'Mướp đắng rừng', 'Cây mướp đắng rừng còn được gọi là khổ qua rừng là một loài cây mọc hoang ở khắp các sườn đồi từ Bắc trí Nam. Mướp đắng hiện là một trong những loại thực phẩm được nhiều người ưa thích. Tuy có vị đắng nhưng loại mướp đặc biệt này vẫn rất hấp dẫn người sử dụng bởi nó rất tốt cho sức khỏe, đặc biệt là những bệnh nhân bị huyết áp cao. Đặc điểm nhận biết mướp đắng rừng và mướp đắng nhà. Mướp đắng rừng có cùng họ với mướp đắng nhà xong có kích thước quả và dây nhỏ hơn, vị đắng cao hơn. Mướp đắng rừng vẫn được người dân vùng cao hái về dùng làm rau ăn hàng ngày. Cây mọc hoang rất nhiều ở các sườn đồi, đến nỗi nhiều lúc người dân còn phải phát quang đi để tránh làm ảnh hưởng tới những cây trồng khác. Ngày nay, nhu cầu sử dụng mướp đắng làm thuốc tăng cao, người dân vùng cao đi hái quả về thái xong phơi khô để làm thuốc. Mướp khô có thể dùng hãm với nước sôi hoặc đun nước uống hàng ngày. Thành phần hóa học: Trong quả có chứa hoạt chất glucozit đắng gọi là momocđixin, đặc biệt trong quả mướp đắng có hàm lượng vitamin B1, C, betain, protein khá cao. Tác dụng của mướp đắng rừng: Tác dụng hạ huyết áp, tác dụng mát gan giải độc, hạ men gan, tác dụng ổn định đường huyết, hỗ trợ điều trị bệnh tiểu đường Cách dùng khổ qua rừng làm thuốc. Cây mướp đắng rừng có thể dùng tươi hoặc dùng khô đều được. Dùng tươi: Dùng lá, dây hoặc quả nấu canh ăn hàng ngày như các loại mướp thông thường. Dùng khô: Lá, quả đem phơi để sử dùng. Liều dùng: 20g/ngày dưới dạng nước sắc hoặc hãm nước sôi uống hàng ngày.', '../images/Qua-muop-dang-rung-300x168.jpg'),
 ('3', 'Nấm lim xanh', 'Nấm lim xanh có rất nhiều tác dụng, một trong những tác dụng quý nhất của vị thuốc này đó là tác dụng hỗ trợ điều trị bệnh tiểu đường. Thành phần nấm lim xanh gồm rất nhiều các loại dược chất không đường, các dược chất này có khả năng chuyển hóa đường huyết trong máu, làm giảm lượng đường huyết giúp bệnh nhân tiểu đường ổn định được đường huyết trong máu. Ngoài ra trong nấm lim xanh còn có cơ chế phục hồi các tế bào ở tuyến tụy, giúp tăng sản sinh ra Insulin (Insulin là một chất quan trọng trong có thể, thiếu insulin đồng nghĩa với bạn bị bệnh tiểu đường). Cơ chế của nấm lim xanh là gián tiếp sản sinh ra insulin. Đây là 1 cơ chế điều trị tiểu đường bền vững hơn hẳn so với các loại thuốc điều trị tiểu đường có bán trên thị trường hiện nay. Các phương pháp điều trị bệnh tiểu đường hiện nay đều nhằm mục đích trung là cung cấp trực tiếp 1 lượng insulin cho người bệnh. Ưu điểm của các phương pháp này là hiệu quả nhanh, xong nhược điểm là: Người bệnh tiểu đường phải phụ thuộc hoàn toàn vào thuốc, về lâu về dài làm sẽ làm vô hiệu hóa chức năng tạo insulin tự nhiên của tuyến tụy. Như vậy nấm lim xanh với bệnh tiểu đường có 2 tác dụng chính sau: Giảm lượng đường trong máu, kích thích tăng sản sinh insulin ở tuyến tụy, tăng cường sức đề kháng, ngăn ngừa biến chứng của bệnh tiểu đường. Đây là 2 tác dụng quý giá nhất của nấm lim xanh trong điều trị tiểu đường, các tác dụng này vượt trội hơn hẳn so với các loại thảo dược được cho là có tác dụng điều trị tiểu đường hiện nay. Ngoài ra hàng trục dược chất quý trong nấm lim xanh còn có công dụng đào thải độc tố trong cơ thể, giúp người bệnh nâng cao sức đề kháng, ngăn ngừa các biến chứng bệnh tiểu đường. Cách dùng nấm lim xanh điều trị tiểu đường\r\nChuẩn bị: Nấm lim xanh khô 20g, nước 300ml. Cách làm: Nấm rửa sạch (Nhất là phần chân nấm), thái mỏng, bỏ vào ấm rồi tráng qua 1 lần bằng nước sôi. Đổ 300ml nước sôi vào ấm hãm trong thời gian 30 phút là dùng được. Cách dùng: Uống trước bữa ăn 30 – 40 phút. Các thời gian dùng thuốc Tây từ 30 phút trở lên. Lưu ý khi dùng nấm lim xanh điều trị tiểu đường: Khi dùng nấm các bạn vẫn phải tuân thủ chế độ ăn kiêng của người bệnh tiểu đường Vận đông, thể dục đều đặn sẽ giúp ích rất nhiều cho việc điều trị bệnh Không bỏ thuốc Tây mà nên kết hợp sử dụng để đem lại hiệu quả điều trị cao nhất.', '../images/gia-nam-lim-xanh-tien-phuoc-1.jpg'),
-('4', 'giảo cổ lam', NULL, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdnFoMZ4ViebcSoxaotHDfaZLMPrig2eRZdsHF_gRzLfzCRMJdpg'),
-('5', 'trà hoa vàng', NULL, 'https://drive.google.com/open?id=1Zc18bqaE7EPEJX12DI-zqRrdqEwO9uuM'),
-('6', 'cây cối xay', NULL, 'https://drive.google.com/open?id=1swm2429Z1bsSEmICwLH2DH9QhMQ6Z05p'),
-('7', 'hạt chuối cô đơn', NULL, 'https://drive.google.com/open?id=17_MCAlNNmuktEuwK6lU9ovoUb9UG1YUO'),
-('8', 'cây bấc đèn', NULL, 'https://drive.google.com/open?id=1FpFVVypKkZs6ZUSbneIZIgmCAs6iBDCJ\r\n'),
-('9', 'dạ minh sa', NULL, 'https://drive.google.com/open?id=1HWEC8RqppoZ_-xbXVnp6xHPpZoAsXcSu');
+('4', 'Giảo cổ lam', '', ''),
+('5', 'trà hoa vàng', '', ''),
+('7', '\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0', NULL, 'https://drive.google.com/open?id=17_MCAlNNmuktEuwK6lU9ovoUb9UG1YUO'),
+('8', '\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0', NULL, 'https://drive.google.com/open?id=1FpFVVypKkZs6ZUSbneIZIgmCAs6iBDCJ\r\n'),
+('9', '\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?\0\0\0?', NULL, 'https://drive.google.com/open?id=1HWEC8RqppoZ_-xbXVnp6xHPpZoAsXcSu');
 
 -- --------------------------------------------------------
 
@@ -235,6 +236,7 @@ ALTER TABLE `benh`
 -- Chỉ mục cho bảng `lienhe`
 --
 ALTER TABLE `lienhe`
+  ADD PRIMARY KEY (`idlienhe`),
   ADD KEY `Email` (`Email`);
 
 --
@@ -276,6 +278,16 @@ ALTER TABLE `thuoc`
 ALTER TABLE `thuocbenh`
   ADD KEY `tenbenh` (`tenbenh`),
   ADD KEY `idthuoc` (`idthuoc`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `lienhe`
+--
+ALTER TABLE `lienhe`
+  MODIFY `idlienhe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
