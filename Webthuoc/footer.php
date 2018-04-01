a160bb357f5cf89c469accf039b5a4ee8efeb3a6
cuongdinh1507 <cuongdz1507@gmail.com>
2018-04-01 18:12:40 +0700

    chỉnh sửa footer

--- Webthuoc/footer.php
+++ Webthuoc/footer.php
@@ -1,12 +1,17 @@
 
-	<div id="footer">
-		<div id="diachi">
-			<font size="5">
-		<p align="">Cây Thuốc Nam</p>
-		<p align="">GS.TS: Nguyễn Quý Hiếu</p>
-		<p align="">Địa Chỉ: Nam Từ Liêm </p>
-		<p align="">Hotline: 1900 0019</p>
-			</font>
+	<div class="footer">
+		<div id="div1">
+			<h1>Giới thiệu về trang web</h1>
+			<p>Được phát triển bởi 3 thành viên</p>
+			<p>Nguyễn Quý Hiếu</p>
+			<p>Đinh Việt Cường</p>
+			<p>Nguyễn Mạnh Cường</p>
+		</div>
+		<div id="div2">
+			<h1>Chi tiết liên hệ</h1>
+			<p>Gmail: cuongdv52@wru.vn</p>
+			<p>Điện thoại liên hệ: 0123456789</p>
+			<p>Địa chỉ: 175 Tây Sơn - Đống Đa - Hà Nội</p>
 		</div>
 	</div>
 </body>
